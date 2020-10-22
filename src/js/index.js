e1c9bcac08966c0b2cdf24bd01dcfe897243309f
derekdong12 <50589954+derekdong12@users.noreply.github.com>
2020-10-22 13:59:00 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
+// function getPkmn() {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/ditto`).then((result) => {
+//         return result.json();
+//     }).then((data) =>{
+//         console.log(data);
+//     })
+// }
 
+const pkmn = "newtwo";
+async function getPkmn() {
+    try {
+        const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`);
+        const data = await result.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+        alert("did you spell that correctly");
+    }
+}
+getPkmn();
