6caa06a4f442b094c1dd615949f8156d1b00f6af
derekdong12 <50589954+derekdong12@users.noreply.github.com>
2020-10-27 14:13:25 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,51 @@ async function getPkmn() {
     }
 }
 getPkmn();
+
+
+const DOMSelectors = {
+    grid: document.querySelector(".movie-grid"),
+};
+
+const key = '';
+
+const query = '';
+
+const init = async function() {
+    try {
+        const response = await fetch(query);
+        const data = await response.json();
+        data.results.forEach((movie) => {
+            DOMSelectors.grid.insertAdjacentHTML("beforehand", `<div class="movie-card">
+            <div class="movie-card-front">
+              <img
+                src="https://image.tmdb.org/t/p/w300/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg"
+                alt=""
+                class="poster"
+              />
+            </div>
+            <div class="movie-card-back">
+              <h3 class="movie-card-header">The Dark Knight</h3>
+              <div class="score-box">
+                <p class="user-score">Community Score</p>
+                <p class="user-score">8.4</p>
+              </div>
+    
+              <div class="release-box">
+                <p class="release-date">Released</p>
+                <p class="release-date">2020-06-12</p>
+              </div>
+    
+              <div class="movie-genres">
+                <li class="movie-genre">Sci-Fi</li>
+                <li class="movie-genre">Fantasy</li>
+                <li class="movie-genre">Horror</li>
+              </div>
+            </div>
+          </div>`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+init();
