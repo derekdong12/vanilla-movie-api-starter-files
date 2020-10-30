05c4c76d35b92acc62b467dda4a59b4f92e3fe2e
derekdong12 <50589954+derekdong12@users.noreply.github.com>
2020-10-29 20:24:24 -0400

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,43 @@
-// function getPkmn() {
-//     fetch(`https://pokeapi.co/api/v2/pokemon/ditto`).then((result) => {
-//         return result.json();
-//     }).then((data) =>{
-//         console.log(data);
-//     })
-// }
-
-const pkmn = "newtwo";
-async function getPkmn() {
-    try {
-        const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`);
-        const data = await result.json();
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-        alert("did you spell that correctly");
-    }
-}
-getPkmn();
-
-
 const DOMSelectors = {
-    grid: document.querySelector(".movie-grid"),
+  grid: document.querySelector(".movie-grid"),
 };
+const key = `1fd276ec57b4baedacae00246e5cf4b7`;
+const query = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=10000&vote_average.gte=8`;
 
-const key = '';
-
-const query = '';
-
-const init = async function() {
-    try {
-        const response = await fetch(query);
-        const data = await response.json();
-        data.results.forEach((movie) => {
-            DOMSelectors.grid.insertAdjacentHTML("beforehand", `<div class="movie-card">
-            <div class="movie-card-front">
-              <img
-                src="https://image.tmdb.org/t/p/w300/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg"
-                alt=""
-                class="poster"
-              />
-            </div>
-            <div class="movie-card-back">
-              <h3 class="movie-card-header">The Dark Knight</h3>
-              <div class="score-box">
-                <p class="user-score">Community Score</p>
-                <p class="user-score">8.4</p>
-              </div>
-    
-              <div class="release-box">
-                <p class="release-date">Released</p>
-                <p class="release-date">2020-06-12</p>
-              </div>
-    
-              <div class="movie-genres">
-                <li class="movie-genre">Sci-Fi</li>
-                <li class="movie-genre">Fantasy</li>
-                <li class="movie-genre">Horror</li>
-              </div>
-            </div>
-          </div>`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
+const init = async function () {
+  try {
+    const response = await fetch(query);
+    const data = await response.json();
+    data.results.forEach((movie) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="movie-card">
+      <div class="movie-card-front">
+        <img
+          src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+          alt=""
+          class="poster"
+        />
+      </div>
+      <div class="movie-card-back">
+        <h3 class="movie-card-header">${movie.original_title}</h3>
+        <div class="score-box">
+          <p class="user-score">Community Score</p>
+          <p class="user-score">${movie.vote_average}</p>
+        </div>
+        <div class="release-box">
+          <p class="release-date">Released</p>
+          <p class="release-date">${movie.release_date}</p>
+        </div>
+        <div class="movie-genres">
+          <div>${movie.genre_ids}</div>
+        </div>
+      </div>
+    </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 init();
